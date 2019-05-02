4e22b9d9b9db9fc2caba81a272860890ceb543b9
Jahangir Iqbal <jiqbal@vibrenthealth.com>
2019-05-02 12:54:22 -0400

    simple hello component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import reactDom from "react-dom";
 
-import "./styles.css";
+const SayHello = props => {
+  return <h1>Hello {props.name}</h1>;
+};
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
-}
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+reactDom.render(<SayHello name="goku" />, document.getElementById("root"));
